1bd6ba25d142034d54dc9973683b161024278f5f
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2019-02-10 01:35:52 +0000

    Added comments

--- cam/opencv_practice.py
+++ cam/opencv_practice.py
@@ -2,16 +2,24 @@
 # References
 #  https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
 
+# helps construct n-dimensional numpy array from camera output
 from picamera.array import PiRGBArray
+
+# For using Raspberry Pi Camera
 from picamera import PiCamera
+
 import time
+
+# computer vision
 import cv2
 
+# set up
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
+# allow camera to warm up
 time.sleep(0.1)
 
 # Captures a frame
@@ -20,9 +28,14 @@ time.sleep(0.1)
 # cv2.imshow("Image", image)
 # cv2.waitKey(0)
 
+# capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw numpy array representing the image
 	image = frame.array
+
+	# show the frame
 	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) 
 
+	# clear the stream
 	rawCapture.truncate(0)
