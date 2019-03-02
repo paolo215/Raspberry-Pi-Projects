49be2c6eedd9ca6ca19ca5009ab0d81981ea695f
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2019-03-02 19:30:48 +0000

    Added threads to get latest frames faster

--- cam/usb_opencv.py
+++ cam/usb_opencv.py
@@ -6,14 +6,19 @@ import datetime
 import sys
 
 from camera import Camera
+from queue import Queue
+from threading import Thread
 
 class USB_OpenCV(Camera):
-    def __init__(self, prefix, resolution):
+    def __init__(self, prefix, resolution, stream=False):
         super().__init__(prefix, resolution)
         self.camera = cv2.VideoCapture(0)
         self.set_resolution(self.resolution)
         self.fourcc = cv2.VideoWriter_fourcc(*"MPEG")
         self.writer = None
+
+        # Only for recording videos
+        self.current_frame = None
 
     def set_resolution(self, resolution):
         # Reference:
@@ -23,21 +28,52 @@ class USB_OpenCV(Camera):
         self.camera.set(3, resolution[0])
         self.camera.set(4, resolution[1])
 
+    def get_frame(self):
+        if self.camera.isOpened():
+            return self.camera.read()
+        return None
+
+    def collect_frames(self):
+        while True:
+            return_value, frame = self.get_frame()
+            if return_value:
+                self.current_frame = frame
+
+    def show_frame(self, image):
+        cv2.imshow("Frame", image)
+        cv2.waitKey(1)
+        
+    def write_frame(self, writer, image):
+        self.writer.write(image)
+        
+    def start_record(self, seconds):
+        t1 = Thread(target=self.collect_frames)
+        t2 = Thread(target=self.record_continuous_save_every, args=(seconds,))
+        threads = [t1, t2]
+
+        for t in threads:
+            t.daemon = True
+            t.start()
+        
+        for t in threads:
+            t.join()
+
+
     def record(self, seconds):
         try:
             start = time.time()
             filename = self.generate_filename("avi", "video")
+            print(filename)
             self.writer = self.createVideoWriter(filename)
             while time.time() - start < seconds \
                 and self.camera.isOpened():
-                return_value, image = self.camera.read()
-                if return_value == True:
-                    self.writer.write(image)
-                    cv2.imshow("Frame", image)
-                    cv2.waitKey(1)     
+                image = self.current_frame
+                if image != None:
+                    self.show_frame(image)
         except KeyboardInterrupt:
             self.stop()
 
+        print("END")
         return None
 
 
@@ -56,7 +92,6 @@ class USB_OpenCV(Camera):
         cv2.destroyAllWindows()
         print("Exiting...")
         sys.exit(1)
-        
 
     def take_picture(self):
         try:
@@ -81,7 +116,6 @@ class USB_OpenCV(Camera):
                     self.take_picture()
         except KeyboardInterrupt:
             self.stop()
-        
 
     def createVideoWriter(self, filename):
         return cv2.VideoWriter(filename, self.fourcc, 30.0, self.resolution)
@@ -89,6 +123,7 @@ class USB_OpenCV(Camera):
 
 if __name__ == "__main__":
     a = USB_OpenCV("test", (640, 480)) 
-    a.record_continuous_save_every(10)
+    #a.record_continuous_save_every(60*10)
+    a.start_record(60)
     #a.take_picture_every(10)
     #a.take_picture()
